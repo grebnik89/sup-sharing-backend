4bf7a409db33cabcc3f28a73d7f48733865248a2
Grebnik89 <116430298+art-just-code@users.noreply.github.com>
2024-12-11 21:58:33 +0300

    add normalizeToObject and populate

--- middlewares/dates.js
+++ middlewares/dates.js
@@ -1,9 +1,17 @@
 const dates = require("../models/dates");
-const normalizeToObject = require("../utils/normalize/normalize");
+//const normalizeToObject = require("../utils/normalize/normalize");
 
 const findAllDates = async (req, res, next) => {
-    const dataArray = await dates.find({}, { _id: 0, user: 0 });
-    req.normalizedDataObject = await normalizeToObject(dataArray);
+    if (req.query["month.name"]) {
+        const dataArray = await dates.findDatesByMonth(req.query["month.name"]);
+        req.normalizedDataObject = dataArray;
+        next();
+        return;
+    }
+
+    const dataArray = await dates.find({}, { _id: 0, user: 0 }).populate("month"); // тут тоже не хотят популэйтиться месяцы
+    req.normalizedDataObject = dataArray;
+    //req.normalizedDataObject = await normalizeToObject(dataArray);
     next();
 };
 
--- models/dates.js
+++ models/dates.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userModel = require("./users");
+const monthModel = require("./monthes");
 
 const datesSchema = new mongoose.Schema({
     date: {
@@ -11,10 +12,21 @@ const datesSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    month: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: monthModel,
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: userModel,
     },
 });
+
+datesSchema.statics.findDatesByMonth = function (month) {
+    return this.find({}, { _id: 0 }).populate("month").populate("user"); // понять почему юзер популэйтится, а месяцы - нет
+    /* .then((dates) => {
+            return dates; //.filter((date) => date.month === month);
+        }); */
+};
 
 module.exports = mongoose.model("dates", datesSchema);
